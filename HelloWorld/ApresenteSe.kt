7130c7b91158de3acda4dd05e9a23ae0a0617a79
Thiago Zupelli <thiago_zupelli@hotmail.com>
2024-02-22 20:25:26 -0300

    Added Apresente-se

---
+++ HelloWorld/ApresenteSe.kt
@@ -0,0 +1,12 @@
+//Declarando variáveis em Kotlin
+
+fun main() {
+    // Declarando variáveis
+
+    var idade = 27 // Idade do usuário
+    var altura = 1.72 // Altura do usuário
+    var inicial = 'T' // Primeira letra do nome do usuário
+
+    // Imprimindo variáveis
+    println("Eu tenho $idade anos e $altura de altura, e meu nome começa com a inicial $inicial.")
+}
